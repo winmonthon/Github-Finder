2dc3dab3b98a89975a3ead76cb6f3f8f9d14bbe7
winmonthon <you@example.com>
2021-09-20 22:38:44 +0700

    update alert

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Search from "../users/Search";
 import Users from "../users/User";
+import Alert from "../layout/Alert";
 
 const Home = () => {
   return (
     <Fragment>
+      <Alert />
       <Search />
       <Users />
     </Fragment>
